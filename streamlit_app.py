8cc2527ea737f4c62272132ed85091a4de0a41f2
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2024-08-27 21:21:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import pandas as pd
+import math
+from pathlib import Path
+import pickle
+st.title("model testing")
+engine_size=st.number_input('engine_size',min_value=0,max_value=10,value=1)
+clylinder=st.number_input('clylinder',min_value=0,max_value=10,value=1)
+fuelcompution=st.number_input('fuelcompution',min_value=0,max_value=10,value=1)
+with open('model (1).pkl', 'rb') as file:
+model = pickle.load(file)
+model.predict([[engine_size,clylinder,fuelcompution]])
+st.write("car of co2 is",output[0][0])
