b2e3cd6f8c34a4108923cb8b8309eadae3331150
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2024-09-29 20:53:24 +0300

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,1 +1,52 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import joblib
+from PIL import Image
+from datetime import datetime, date
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
 
+
+# Setting page configuration
+st.set_page_config(page_title="Acustomer segmenation", page_icon="✈️", layout='wide')
+
+
+# Loading data
+df = pd.read_csv('cleaned_df.csv')
+
+
+with st.sidebar:
+
+    st.sidebar.image('R.jpg')
+    st.sidebar.subheader("This dashboard for Indian Aviation Flights Fare aimed at predicting the prices of flight tickets")
+    st.sidebar.write("")
+    
+    data = df.copy()
+    source = st.sidebar.selectbox("Departure City", ['All'] + list(data['Source'].unique()))
+    # data after Source selection
+    if source != 'All':
+        data = data[data['Source'] == source]
+    
+    destination = st.sidebar.selectbox("Arrival City", ['All'] + list(data['Destination'].unique()))
+    # data after Source and Destination selection
+    if destination != 'All':
+        data = data[data['Destination'] == destination]
+
+    duration = data[(data['Source'] == source) & (data['Destination'] == destination)]
+    
+    airline = st.sidebar.selectbox("Airline Carrier", ['All'] + list(data['Airline'].unique()))
+    # data after Source and Destination and Month and Day selection and departure hour selection and airline selection
+    if airline != 'All':
+        data = data[data['Airline'] == airline]
+
+    add_info = st.sidebar.selectbox("Additional Services", ['All'] + list(data['Additional_Info'].unique()))
+
+    filter_box = st.sidebar.selectbox("Filter Prices on", [None, 'Day', 'Month', 'Dep_Hour'])
+
+
+
+    st.sidebar.write("")
+    st.sidebar.markdown("Made by [Hussein zayed](https://github.com/HusseinZayed)")
